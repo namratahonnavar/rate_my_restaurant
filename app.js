a0ca6c49b3aa0218d863940bc199342be4d613dc
namratahonnavar <namrata.nh@gmail.com>
2014-06-27 22:53:38 -0600

    can provide ratings

--- app.js
+++ app.js
@@ -1,6 +1,69 @@
 $(document).ready(function(){
-    $('img').click(function () {
-      $('div').empty().append('<img src="images/2.png">');    
-    })
+	//for div 1
+    $('.first_rest #starz1').click(function(){
+    	$('.first_rest').empty().append('<img src="images/1.png">'); 
+    	
+    });
+    $('.first_rest #starz2').click(function(){
+    	$('.first_rest').empty().append('<img src="images/2.png">'); 
+    	
+    });
+    $('.first_rest #starz3').click(function(){
+    	$('.first_rest').empty().append('<img src="images/3.png">'); 
+    	
+    });
+    $('.first_rest #starz4').click(function(){
+    	$('.first_rest').empty().append('<img src="images/4.png">');
+    	 
+    });
+    $('.first_rest #starz5').click(function(){
+    	$('.first_rest').empty().append('<img src="images/5.png">');  
+    	alert("your rating has been registered")
+    });
+
+//for div 2
+		$('.second_rest #starz1').click(function(){
+    	$('.second_rest').empty().append('<img src="images/1.png">'); 
+    	
+    });
+    $('.second_rest #starz2').click(function(){
+    	$('.second_rest').empty().append('<img src="images/2.png">'); 
+    	
+    });
+    $('.second_rest #starz3').click(function(){
+    	$('.second_rest').empty().append('<img src="images/3.png">'); 
+    	
+    });
+    $('.second_rest #starz4').click(function(){
+    	$('.second_rest').empty().append('<img src="images/4.png">');
+    	 
+    });
+    $('.second_rest #starz5').click(function(){
+    	$('.second_rest').empty().append('<img src="images/5.png">');  
+    	alert("your rating has been registered")
+    });
+
+    //for div3
+    $('.third_rest #starz1').click(function(){
+    	$('.third_rest').empty().append('<img src="images/1.png">'); 
+    	
+    });
+    $('.third_rest #starz2').click(function(){
+    	$('.third_rest').empty().append('<img src="images/2.png">'); 
+    	
+    });
+    $('.third_rest #starz3').click(function(){
+    	$('.third_rest').empty().append('<img src="images/3.png">'); 
+    	
+    });
+    $('.third_rest #starz4').click(function(){
+    	$('.third_rest').empty().append('<img src="images/4.png">');
+    	 
+    });
+    $('.third_rest #starz5').click(function(){
+    	$('.third_rest').empty().append('<img src="images/5.png">');  
+    	
+    });
+
 });
 
